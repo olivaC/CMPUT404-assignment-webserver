e94939da1be33553aa1a70546108c4569ddce046
olivaC <coliva@ualberta.ca>
2019-01-20 10:32:27 -0700

    Fix redirects

--- server.py
+++ server.py
@@ -40,30 +40,27 @@ CSS = "Content-Type: text/css\r\n\r\n"
 class MyWebServer(socketserver.BaseRequestHandler):
 
     def handle(self):
-        start_os = os.path.abspath("www/")  # Used to find CSS files from the www/ directory
         self.data = self.request.recv(1024).strip().decode("utf-8")
-        # print(self.data.split("\r\n")[0])
         if self.data.startswith('GET'):
-            print(self.data.split("\r\n")[0])
-            # print(self.data.split("\r\n"))
-            url_file, error = decompose_request(self.data)
-            if error:
-                self.request.sendall(bytearray(ERROR404, 'utf-8'))
-                pass
-            try:
-                open(url_file)
-            except FileNotFoundError:
-                print("File not found")
-                self.request.sendall(bytearray(ERROR404, 'utf-8'))
-            if url_file.endswith("index.html"):
-                sendall_html = generate_sendall(url_file, 'html')
-                self.request.sendall(bytearray(sendall_html, 'utf-8'))
-            if url_file.endswith('.css'):
-                sendall_css = generate_sendall(url_file, 'css')
-                self.request.sendall(bytearray(sendall_css, 'utf-8'))
+            print("Request: " + self.data.split("\r\n")[0])
+            url_file, code = decompose_request(self.data)
+            print("Sending Request Type: " + code + "\n")
+            if code == '200':
+                if url_file.endswith("index.html"):
+                    sendall_html = generate_sendall(url_file, 'html')
+                    self.request.sendall(sendall_html.encode())
+                if url_file.endswith('.css'):
+                    sendall_css = generate_sendall(url_file, 'css')
+                    self.request.sendall(sendall_css.encode())
+            elif code == '301':
+                sendall_redirect = generate_sendall(url_file, 'redirect')
+                self.request.sendall(sendall_redirect.encode())
+            else:
+                self.request.sendall(ERROR404.encode())
+
         else:
             # Return status code 405 Method not allowed for (POST/PUT/DELETE)
-            self.request.sendall(bytearray(ERROR405, 'utf-8'))
+            self.request.sendall(ERROR405.encode())
 
 
 # ------------ User Defined Functions ------------#
@@ -74,43 +71,47 @@ def decompose_request(data):
     :param data: the url
     :return: file path
     """
-    start_os = os.path.abspath("www/")  # Used to find CSS files from the www/ directory
     url = data.split("\r\n")[0].split(" ")[1]
     prefix = 'www'
     delimiter = '/'
     spl = url.split("/")
     length = len(spl)
-    print(spl)
-    print(length)
     if spl[length - 1] == "":
-        spl[length - 1] = "index.html"
+        return existing_file(spl, delimiter, prefix, 1)
     elif not spl[length - 1].endswith(".html") and not spl[length - 1].endswith(".css"):
-        spl.append("index.html")
-    elif spl[length - 1].endswith(".css"):  # Find the CSS file, assuming that each CSS file is uniquely named.
-        abs_path = find(spl[length - 1], start_os)
-        abs_file = abs_path.split('www/')
-        print(abs_file[1])
-        if str(abs_file[1]).count('/') != length:
-         return prefix + delimiter + str(abs_file[1]), True
+        return existing_file(spl, delimiter, prefix, 2)
+    elif spl[length - 1].endswith(".css"):  # Handle serving css
+        return existing_file(spl, delimiter, prefix, 3)
+
+    return prefix + delimiter.join(spl), '200'
+
+
+def existing_file(spl, delimiter, prefix, if_type):
+    """
+    Checks if the file requested exists, and whether or not to send a 200, 301 or 404
+
+    :param spl: The url split into a list
+    :param delimiter: '/'
+    :param prefix: www
+    :param if_type: Integer
+    :return: data, request type
+    """
+    if if_type in (1, 2):
+        file_exists = os.path.isfile('www/{}/index.html'.format("/".join(spl)))
+        if file_exists:
+            if if_type == 1:
+                return 'index.html', '301'
+            else:
+                spl.append("index.html")
+                return delimiter.join(spl), '301'
         else:
-            return prefix + delimiter + str(abs_file[1]), False
-
-    return prefix + delimiter.join(spl), False
-
-
-def find(name, path):
-    """
-    Finds a file based on the directory given as well as the file name. Returns the path.
-
-    Taken from: https://stackoverflow.com/questions/1724693/find-a-file-in-python on January 15/19
-    Author: https://stackoverflow.com/users/97828/nadia-alramli
-    :param name: the file name
-    :param path: path
-    :return: file path
-    """
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
+            return "", '404'
+    if if_type == 3:
+        file_exists = os.path.isfile('www/{}'.format("/".join(spl)))
+        if file_exists:
+            return prefix + delimiter + "/".join(spl)[1:], '200'
+        else:
+            return "", '404'
 
 
 def generate_sendall(file, req_type):
@@ -118,11 +119,14 @@ def generate_sendall(file, req_type):
     Adds the correct heading to the HTML/CSS files found in the www folder
 
     :param file: the html/css file to be opened and read
-    :param req_type: either html or css
+    :param req_type: either html/css/redirect
     :return: A complete string including the correct headings
     """
     if req_type == 'html':
         sendall = OK + HTML
+    elif req_type == 'redirect':
+        sendall = "HTTP/1.1 301 Moved Permanently\r\nLocation: {}\n".format(file)
+        return sendall
     else:
         sendall = OK + CSS
     with open(file, 'r', encoding='utf-8') as infile:
